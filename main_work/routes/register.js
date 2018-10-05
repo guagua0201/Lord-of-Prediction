b1e55cf37273300f5b5d0d36f46fbb5c87f44c0d
justinQAQ <dancinglinkxalgorithm@gmail.com>
2018-10-06 03:24:52 +0800

    Working on register page

--- main_work/routes/register.js
+++ main_work/routes/register.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/register', function(req, res, next) {
-  res.render('register', { route: 'register' });
+var db = require('../config/db');
+var sql = require('mssql');
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {  
+  res.render('/', {
+    route: 'register'
+  });
+});
+
+/* POST register page. */
+router.post('/register', function(req, res, next) {
+  sql.connect(db, function(err) {
+    if (err)
+      console.log(err);
+
+    /* 
+     * do some check about input 
+     * 1. no same username
+     * 2. password == confirmed_password
+     * 3. check email with regular expression
+     * 4. username only include char, '_', and digit
+     * check inside html form box 
+     * https://www.w3schools.com/js/tryit.asp?filename=tryjs_validation_js
+    */
+    
+    var request = new sql.Request();
+    request.input('nickname', sql.NVarChar(20), req.body.nickname)
+      .input('email', sql.NVarChar(50), req.body.email)
+      .input('gender', sql.Bit, req.body.gender)
+      .input('username', sql.NVarChar(20), req.body.username)
+      .input('password', sql.NVarChar(20), req.body.password)
+      .query('insert into UserList (Nickname, Email, Gender, Username, Password) values(@nickname, @email, @gender, @username, @password', function(err, result) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        sql.close();
+        req.redirect('/');
+      });
+  });
 });
 
 module.exports = router;
