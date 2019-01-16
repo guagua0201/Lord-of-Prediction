16629132f2d2300f0e78cf92bf235cf9fdfa96a6
justin0u0 <dancinglinkxalgorithm@gmail.com>
2019-01-17 01:30:42 +0800

    test

--- main/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('./smarty-3.1.33/libs/Smarty.class.php');
-require_once('./configs/config.php');
-
-$smarty = new Smarty();
-$smarty->template_dir = APP_REAL_PATH . "/templates/";
-$smarty->compile_dir = APP_REAL_PATH . "/templates_c/";
-?>
