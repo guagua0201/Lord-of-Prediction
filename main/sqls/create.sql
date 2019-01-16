db53dadbf8ffcaab0d4a32333bfce556803a30b1
justin0u0 <dancinglinkxalgorithm@gmail.com>
2019-01-17 01:22:01 +0800

    add table: Comment so modified create.sql

--- main/sqls/create.sql
+++ main/sqls/create.sql
@@ -39,3 +39,12 @@ CREATE TABLE IF NOT EXISTS `Category` (
 ) ENGINE = InnoDB;
 
 -- INSERT INTO `Category` (`id`, `name`, `description`) VALUES (NULL, '系統', NULL);
+
+CREATE TABLE IF NOT EXISTS `Comment` ( 
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`article_id` INT NOT NULL , 
+	`user_id` INT NOT NULL , 
+	`content` VARCHAR(5000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+	`create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
