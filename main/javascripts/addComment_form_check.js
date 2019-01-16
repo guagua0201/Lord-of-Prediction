152d41b6249bd6a6cf3fcca1fd323ca23fec424f
justin0u0 <dancinglinkxalgorithm@gmail.com>
2019-01-17 04:36:12 +0800

    Finish add Comment

--- main/javascripts/addComment_form_check.js
+++ main/javascripts/addComment_form_check.js
@@ -1,9 +1,12 @@
 function checkForm(form) {
 	var valid = true;
 	console.log(CKEDITOR.instances.editor.getData());
+ 	document.getElementById('content').setAttribute('value', CKEDITOR.instances.editor.getData());
+	// console.log(document.getElementsByTagName("content")[0].value);
 	if (CKEDITOR.instances.editor.getData().length == 0) {
 		document.getElementById('content_notice').removeAttribute('hidden');
 		valid = false;
 	}
+	// return false;
 	return valid;
 }
