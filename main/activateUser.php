48fd45f08a4a36049f19356e7540a31e4914288e
justin <dancinglinkxalgorithm@gmail.com>
2019-05-14 17:46:31 +0800

    Modify(activateUser.php): fitting new error handler

--- main/activateUser.php
+++ main/activateUser.php
@@ -4,8 +4,9 @@ include_once('isLogin.php');
 
 if (is_numeric($_GET['id']) && isset($_GET['key'])) {
 	$link = mysqli_connect(db_host, db_user, db_password, db_name);
-	if (!$link)
-		header('error.php?error_code=106');
+	if (!$link) {
+		throw_error("300", mysqli_connect_error());
+	}
 	$sql = "SELECT id FROM User WHERE `id` = '" . $_GET['id'] . "' AND `status` = 'N' AND `verify_key` = '" . $_GET['key'] . "'";
 	if ($result = mysqli_query($link, $sql)) {
 		if (mysqli_num_rows($result) == 1) {
@@ -14,14 +15,16 @@ if (is_numeric($_GET['id']) && isset($_GET['key'])) {
 			if (mysqli_query($link, $sql2)) {
 				$smarty->assign('success', true);
 			} else {
-				header('error.php?error_code=101');
+				throw_error("301", "");
 			}
 		} else {
 			$smarty->assign('success', false);
 		}
 	} else {
-		header('error.php?error_code=101');
+		throw_error("301", "");
 	}
 	$smarty->display('activateUser.tpl');
+} else {
+	throw_error("404", "");
 }
 ?>
