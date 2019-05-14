d8a0142cb6db01111360eb26b4f07eea5aa5e8be
justin <dancinglinkxalgorithm@gmail.com>
2019-05-14 17:57:17 +0800

    Modify(buyPredict.php): fitting new error handler

--- main/buyPredict.php
+++ main/buyPredict.php
@@ -2,30 +2,37 @@
 include_once('main.php');
 include_once('isLogin.php');
 
-if (isset($_POST['user_id']) && isset($_POST['predict_id'])) {
+if (isset($_POST['user_id']) && !empty($_POST['user_id']) && isset($_POST['predict_id']) && !empty($_POST['predict_id'])) {
 	if ($_POST['user_id'] !== $_SESSION['user_id']) {
-		$smarty->assign('error', 'Session expired!');
-		$smarty->display('error.tpl');
+		throw_error("103", "");
 	} else {
 		$link = mysqli_connect(db_host, db_user, db_password, db_name);
 		if (!$link) {
-			die("Connection failed " . mysqli_connect_error());
+			throw_error("300", mysqli_connect_error());
 		}
 
 		$sql = "SELECT `money` FROM User WHERE id = '" . $_POST['user_id'] . "'";
-		if ($result = mysqli_query($link, $sql))
+		if ($result = mysqli_query($link, $sql)) {
 			$user_info = mysqli_fetch_assoc($result);
+		} else {
+			throw_error("301", "");
+		}
 
-		$sql2 = "SELECT `price` FROM Predict WHERE id = '" . $_POST['predict_id'] . "'";
-		if ($result = mysqli_query($link, $sql2))
+		$sql = "SELECT `price` FROM Predict WHERE id = '" . $_POST['predict_id'] . "'";
+		if ($result = mysqli_query($link, $sql)) {
 			$predict_info = mysqli_fetch_assoc($result);
+		} else {
+			throw_error("301", "");
+		}
 
 		if ($user_info['money'] >= $predict_info['price']) {
 			$left = $user_info['money'] - $predict_info['price'];
-			$sql3 = "UPDATE User SET `money` = '$left' WHERE id = '" . $_POST['user_id'] . "'";
-			mysqli_query($link, $sql3);
-			$sql4 = "INSERT INTO BuyPredict (user_id, predict_id) VALUES ('" . $_POST['user_id'] . "', '" . $_POST['predict_id'] . "')";
-			mysqli_query($link, $sql4);
+			$sql = "UPDATE User SET `money` = '$left' WHERE id = '" . $_POST['user_id'] . "'";
+			if (!mysqli_query($link, $sql))
+				throw_error("301", "");
+			$sql = "INSERT INTO BuyPredict (user_id, predict_id) VALUES ('" . $_POST['user_id'] . "', '" . $_POST['predict_id'] . "')";
+			if (!mysqli_query($link, $sql))
+				throw_error("301", "");
 			echo 'true';
 		} else {
 			echo 'false';
@@ -33,7 +40,6 @@ if (isset($_POST['user_id']) && isset($_POST['predict_id'])) {
 		mysqli_close($link);
 	}
 } else {
-	$smarty->assign('error', 'Permission Denied!');
-	$smarty->display('error.tpl');
+	throw_error('401', "");
 }
 ?>
