cbeeb8ae91dc46f38a652c44829b1e8d48b5e42a
justin <dancinglinkxalgorithm@gmail.com>
2019-05-14 09:26:16 +0800

    Fix(userProfile.php): add comment, fix styling

--- main/userProfile.php
+++ main/userProfile.php
@@ -18,6 +18,7 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
 		} else {
 			mysqli_close($link);
 			header('Location: error.php?error_code=101');
+			exit(0);
 		}
 	}
 	$smarty->assign('user_info', $user_info);
@@ -27,31 +28,34 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
 	$show = $_GET['show'];
 
 	if ($show == 'predict') {
-		$sql = "SELECT id, name FROM Class WHERE id = 2 OR id = 3 OR id = 7 ORDER BY id";
+		// Get classes & categories
+		$sql = "SELECT `id`, `name` FROM `Class` WHERE `id` = 2 OR `id` = 3 OR `id` = 7 ORDER BY `id`";
 		$classes = array();
 		if ($result = mysqli_query($link, $sql)) {
 			while ($row = mysqli_fetch_assoc($result))
 				$classes[] = $row;
 		}
-		$sql2 = "SELECT id, name, class_id FROM Category WHERE 1";
+		$sql = "SELECT `id`, `name`, `class_id` FROM `Category` WHERE 1";
 		$categories = array();
-		if ($result = mysqli_query($link, $sql2)) {
+		if ($result = mysqli_query($link, $sql)) {
 			while ($row = mysqli_fetch_assoc($result))
 				$categories[] = $row;
 		}
 		$smarty->assign('classes', $classes);
 		$smarty->assign('categories', $categories);
 
+		// Get category id
 		if (!isset($_GET['category_id']) || empty($_GET['category_id']))
 			$_GET['category_id'] = 7;
 		$category_id = $_GET['category_id'];
 
-		$sql3 = "SELECT game_id, predict, predict_flag FROM Predict WHERE user_id = '" . $_GET['user_id'] . "' AND (predict_flag = '1' OR predict_flag = '2') AND category_id = '$category_id'";
+		// Ger user history predict
+		$sql = "SELECT `game_id`, `predict`, `predict_flag` FROM `Predict` WHERE `user_id` = '" . $_GET['user_id'] . "' AND (`predict_flag` = 1 OR `predict_flag` = 2) AND `category_id` = '$category_id'";
 		$history_predict = array();
-		if ($result = mysqli_query($link, $sql3)) {
+		if ($result = mysqli_query($link, $sql)) {
 			while ($row = mysqli_fetch_assoc($result)) {
-				$sql4 = "SELECT `game_datetime`, `h_name`, `a_name` FROM Game WHERE id = '" . $row['game_id'] . "'";
-				$game_info = mysqli_fetch_assoc(mysqli_query($link, $sql4));
+				$sql2 = "SELECT `game_datetime`, `h_name`, `a_name` FROM `Game` WHERE `id` = '" . $row['game_id'] . "'";
+				$game_info = mysqli_fetch_assoc(mysqli_query($link, $sql2));
 				$row['game_date'] = $game_info['game_datetime'];
 				$row['game_home_team'] = $game_info['h_name'];
 				$row['game_away_team'] = $game_info['a_name'];
@@ -60,20 +64,23 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
 		}
 		$smarty->assign('history_predict', $history_predict);
 
+		// Get bought predicts
 		$predicts = array();
 		$already_buy = array();
 		if ($_GET['user_id'] != $_SESSION['user_id']) {
-			$sql5 = "SELECT predict_id FROM BuyPredict WHERE user_id = '" . $_SESSION['user_id'] . "'";
-			if ($result = mysqli_query($link, $sql5)) {
+			$sql = "SELECT predict_id FROM BuyPredict WHERE user_id = '" . $_SESSION['user_id'] . "'";
+			if ($result = mysqli_query($link, $sql)) {
 				while ($row = mysqli_fetch_assoc($result))
 					$already_buy[] = $row['predict_id'];
 			}
 		}
-		$sql5 = "SELECT id, game_id, predict, price FROM Predict WHERE user_id = '" . $_GET['user_id'] . "' AND predict_flag = '0' AND category_id = '$category_id'";
-		if ($result = mysqli_query($link, $sql5)) {
+
+		// Get user's predicts
+		$sql = "SELECT `id`, `game_id`, `predict`, `price` FROM `Predict` WHERE `user_id` = '" . $_GET['user_id'] . "' AND `predict_flag` = 0 AND `category_id` = '$category_id'";
+		if ($result = mysqli_query($link, $sql)) {
 			while ($row = mysqli_fetch_assoc($result)) {
-				$sql6 = "SELECT `game_datetime`, `h_name`, `a_name` FROM Game WHERE id = '" . $row['game_id'] . "'";
-				$game_info = mysqli_fetch_assoc(mysqli_query($link, $sql6));
+				$sql2 = "SELECT `game_datetime`, `h_name`, `a_name` FROM `Game` WHERE `id` = '" . $row['game_id'] . "'";
+				$game_info = mysqli_fetch_assoc(mysqli_query($link, $sql2));
 				$row['game_date'] = $game_info['game_datetime'];
 				$row['game_home_team'] = $game_info['h_name'];
 				$row['game_away_team'] = $game_info['a_name'];
@@ -83,14 +90,16 @@ if (isset($_GET['user_id']) && !empty($_GET['user_id'])) {
 		}
 		$smarty->assign('predicts', $predicts);
 
-		$sql7 = "SELECT failure, success, rating FROM Rating WHERE user_id = '" . $_GET['user_id'] . "' AND category_id = '$category_id'";
-		$result = mysqli_query($link, $sql7);
+		// Get user's rating
+		$sql = "SELECT `failure`, `success`, `rating` FROM `Rating` WHERE `user_id` = '" . $_GET['user_id'] . "' AND `category_id` = '$category_id'";
+		$result = mysqli_query($link, $sql);
 		if (mysqli_num_rows($result))
 			$smarty->assign('rating', mysqli_fetch_assoc($result));
 	} else if ($show == 'edit') {
 		if ($_GET['user_id'] != $_SESSION['user_id']) {
 			mysqli_close($link);
 			header('Location: error.php?error_code=103');
+			exit(0);
 		}
 		$sql = "SELECT `id`, `username`, `password`, `nickname`, `email`, `gender`, `image` FROM User WHERE id = '" . $_SESSION['user_id'] . "'";
 		$result = mysqli_query($link, $sql);
