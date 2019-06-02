699e2336d49022609ac3991a5dc3974450c929a9
guagua0201 <tobyupu@gmail.com>
2019-06-02 18:24:01 +0800

    add a lot function to newProfile.js

--- main/javascripts/newProfile.js
+++ main/javascripts/newProfile.js
@@ -1,6 +1,7 @@
 var engine,scene;
 console.log('hi');
 var canvas = document.getElementById("renderCanvas");
+var globalMesh = []
 
 var createScene = async function(){
 	console.log('createScene');
@@ -31,7 +32,22 @@ var createScene = async function(){
         light.position = camera.position;
     });
 
+    set3DButtonSelf(scene);
+
+    var grassMaterial = new BABYLON.StandardMaterial(name + "bawl", scene);
+    var grassTexture = new BABYLON.GrassProceduralTexture(name + "textbawl", 1024, scene);
+    grassMaterial.ambientTexture = grassTexture;
+    var nowColor = grassMaterial.diffuseColor;
+    grassMaterial.diffuseColor = new BABYLON.Color3(nowColor.r*8,nowColor.g*8,nowColor.b*8);
+    var roundGround = BABYLON.MeshBuilder.CreateDisc("disc", {radius: 500, arc: Math.PI, tessellation: 1000, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
+    roundGround.material = grassMaterial;
+    roundGround.rotation.x = Math.PI/2;
+
+    set3DButtonSelf(scene);
+
     await getSelf(scene);
+
+    await makeDressUpPlane(scene);
 
 
 
@@ -58,10 +74,258 @@ var newScene = async function(){
 
 
 function dressUpAnimation(){
-
+	console.log('dressup');
 }
 function closeDressUpAnimation(){
-
+	console.log('close dressup');
 }
 
+
+var makeDressUpPlane = async function(){
+	var menuPlane = BABYLON.MeshBuilder.CreatePlane("menuPlane",{width:1000, height:500},scene);
+    menuPlane.position = new BABYLON.Vector3(0,230,1000);
+    menuPlane.rotate(BABYLON.Axis.Y,Math.PI,BABYLON.Space.LOCAL);
+
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(menuPlane);
+
+    var menuRec = new BABYLON.GUI.Rectangle();
+    
+    menuRec.height = 900 + 'px';
+    menuRec.width =  900 + 'px';
+    //menuRec.scaleY =
+    menuRec.cornerRadius = 20;
+    menuRec.background = "white";
+    menuRec.alpha = 1;
+    menuRec.color = "#666666";
+    menuRec.thickness = 2;
+    menuRec.zIndex = 0;
+    
+    advancedTexture.addControl(menuRec);
+
+    var createRectangle = function() {
+        var rect1 = new BABYLON.GUI.Rectangle();
+        rect1.width = 0.2;
+        rect1.height = "40px";
+        rect1.cornerRadius = 20;
+        rect1.color = "Orange";
+        rect1.thickness = 0;
+        rect1.background = "green";
+        advancedTexture.addControl(rect1);    
+        return rect1;
+    }
+
+    var menuRecImg = new BABYLON.GUI.Image("menuRecImg","images/dressUp/bg2.png");
+    menuRecImg.zIndex = 2;
+    menuRec.addControl(menuRecImg);
+
+    var cate = []
+    for(var i=1;i<=5;i++){
+        cate[i] = BABYLON.GUI.Button.CreateImageOnlyButton("cate" + i.toString(10), "images/dressUp/cate" + i.toString(10) + ".png");
+        
+        cate[i].left = i*100-466;
+        cate[i].top = -325;
+        cate[i].width = 0.1;
+        cate[i].zIndex = 1;
+        cate[i].thickness = 0;
+        cate[i].height = 0.08;
+        if(i==1) cate[i].top = -323;
+        cate[i].id = i;
+        //cate[i].AllowAlphaInheritance = true;
+        cate[i].pointerDownAnimation = function(){
+            for(var i=1;i<=5;i++){
+                advancedTexture.removeControl(cate[i]); 
+                if(i==this.id){
+                    cate[i].top = -323;
+                    cate[i].zIndex = 3;
+                }
+                else{
+                    cate[i].top = -325;
+                    cate[i].zIndex = 1;
+                }
+                //console.log('cate ',i," = ",cate[i]);
+                
+                advancedTexture.addControl(cate[i]); 
+                advancedTexture.removeControl(menuRec);
+                advancedTexture.addControl(menuRec);
+            }
+            this.scaleX = this.scaleY = 1;
+        }
+        advancedTexture.addControl(cate[i]); 
+    }
+
+
+    
+
+    var closeButton = BABYLON.GUI.Button.CreateImageOnlyButton("cate" + i.toString(10), "images/dressUp/closeButton.png");
+
+    closeButton.left = 410;
+    closeButton.top = -380;
+    closeButton.zIndex = 3;
+    closeButton.thickness = 0;
+    closeButton.width = 0.035;
+    closeButton.height = 0.06;
+    closeButton.pointerDownAnimation = function(){
+        scene.registerBeforeRender(closeMenuAnimation);
+    };
+    advancedTexture.addControl(closeButton);
+
+    productID = [];
+
+    for(var i=1;i<=5;i++) {
+        productID[i] = [];
+        for(var j=0;j<10;j++){
+            productID[i][j] = j+1;
+        }
+        
+    }
+    
+    var nowCate = 2;
+
+    var blocks = []
+    var blockImg = []
+    for(var i=0;i<2;i++){
+        for(var j=0;j<5;j++){
+            var id = i*5+j;
+            blocks[id] = BABYLON.GUI.Button.CreateImageOnlyButton("cate" + i.toString(10), "images/dressUp/block.png");
+
+            blocks[id].left = -320 + j*160;
+            blocks[id].top = -50 + i*250;
+            blocks[id].zIndex = 3;
+            blocks[id].thickness = 0;
+            blocks[id].width = 0.12;
+            blocks[id].height = 0.18;
+            advancedTexture.addControl(blocks[id]);
+
+            nowProduct = getProduct(id);
+            blockImg[id] = BABYLON.GUI.Button.CreateImageOnlyButton("block" + id.toString(10),"images/product/" + (productID[nowCate][id]).toString(10) + ".png");
+            blockImg[id].left = -320 + j*160;
+            blockImg[id].top = -50 + i*250;
+            blockImg[id].zIndex = 4;
+            blockImg[id].thickness = 0;
+            blockImg[id].width = 0.12;
+            blockImg[id].height = 0.18;
+            blockImg[id].id = productID[nowCate][id];
+            
+
+            blockImg[id].pointerDownAnimation = function(){
+                console.log('click ',this.id);
+                wearCloth(this.id);
+            }
+            advancedTexture.addControl(blockImg[id]);
+
+            
+        }
+    }
+
+    var lastPage = BABYLON.GUI.Button.CreateImageOnlyButton("lastPage", "images/dressUp/leftTriangle.png");
+    var nextPage = BABYLON.GUI.Button.CreateImageOnlyButton("nextPage", "images/dressUp/rightTriangle.png");
+
+    lastPage.left = -400;
+    lastPage.top = 80;
+    lastPage.zIndex = 3;
+    lastPage.thickness = 0;
+    lastPage.width = 0.015;
+    lastPage.height = 0.06;
+    lastPage.pointerDownAnimation = function(){
+        console.log("lastPage!");
+    };
+    advancedTexture.addControl(lastPage);
+
+    nextPage.left = 400;
+    nextPage.top = 80;
+    nextPage.zIndex = 3;
+    nextPage.thickness = 0;
+    nextPage.width = 0.015;
+    nextPage.height = 0.06;
+    nextPage.pointerDownAnimation = function(){
+        console.log("nextPage!");
+    };
+    advancedTexture.addControl(nextPage);	
+}
+
+set3DButtonSelf = async function(scene){
+        var manager = new BABYLON.GUI.GUI3DManager(scene);
+
+        var panel = new BABYLON.GUI.SpherePanel();
+        panel.margin = 60;
+
+        manager.addControl(panel);
+        anchor.rotation = new BABYLON.Vector3(0,1.95, 0);
+        // console.log(anchor);
+        panel.linkToTransformNode(anchor);
+        panel.position.z = -1.5;
+        panel.position.x = 0;
+        panel.position.y+=100;
+        panel.radius = 200;
+
+        // console.log(panel);
+
+        // Let's add some buttons!
+
+        buttonText = ["設定個人頁面\n1","追隨此用戶\n2","我的追隨\n3","我的商品\n4","購買配件\n5","換裝\n6"];
+
+        var addButton = function(index) {
+            var button = new BABYLON.GUI.HolographicButton("orientation");
+            panel.addControl(button);
+            button.scaling.x=75;
+            button.scaling.y=50;
+            button.scaling.z=50;
+            //console.log(button);
+            //button.text = text;
+            var text1 = new BABYLON.GUI.TextBlock();
+            
+            text1.fontstyle = 'bold'
+            text1.fontFamily = "Verdana";
+            text1.textVerticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER
+            text1.width = 3;
+            text1.text = buttonText[index];
+            text1.color = "White";
+            text1.fontSize = 40;
+            button.content = text1;
+            //button.isVisible = false;
+
+            button.pointerEnterAnimation = () => {
+                button.scaling.x=150;
+                button.scaling.y=100;
+                button.scaling.z=100;
+
+
+            }
+            button.pointerOutAnimation = () => {
+
+                button.scaling.x=75;
+                button.scaling.y=50;
+                button.scaling.z=50;    
+            }
+            button.pointerDownAnimation = function() {
+                
+                if(index == 4){
+                    window.location.pathname = '/../shop.php';
+                }
+                if(index == 5){
+                    // 換裝
+                    for(var i=0;i<6;i++){
+                        backButton[i].isVisible = false;
+                    }
+                    console.log('here');
+                    scene.registerBeforeRender(dressUpAnimation);
+                }
+                if(index == 3){
+                    console.log(globalMesh['body']);
+                }
+                
+            }
+            return button
+        }
+
+        panel.blockLayout = true;
+        
+        for (var index = 0; index < 6; index++) {
+            backButton[index] = addButton(index);    
+        }
+        panel.blockLayout = false;
+    }    
+
+
 newScene();
+
